f6911cc0c80bddbde51ce6c7a448c8ef0b1e0903
pnostimos <pnostimos@users.noreply.github.com>
2014-06-10 12:44:03 +0100

    Create FlatLikeTestForLoggedInUser.java

---
+++ src/test/java/com/flatlike/prototype/FlatLikeTestForLoggedInUser.java
@@ -0,0 +1,108 @@
+package com.flatlike.prototype;
+
+import com.flatlike.prototype.core.FlatLikeEngine;
+import com.flatlike.prototype.model.Property;
+import com.flatlike.prototype.model.Review;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+
+public class FlatLikeTestForLoggedInUser {
+
+    private FlatLikeEngine flatLikeEngine;
+
+    @Before
+    public void setup() {
+        flatLikeEngine = new FlatLikeEngine();
+    }
+
+   @Test
+    public void as_a_user_that_wants_to_sign_in_I_should_be_able_to_login_through_facebook_or_through_registration_form() {
+
+    }
+    
+    @Test
+    public void as_a_signedin_user_I_should_be_able_to_retrieve_a_list_of_properties_for_a_given_post_code() {
+
+        List<Property> properties = flatLikeEngine.getProperties("SE16DB");
+
+        assertThat(properties, is(notNullValue()));
+    }
+
+    @Test
+    public void a_signedin_user_should_be_able_to_retrieve_the_most_recent_overall_reviews() {
+        List<Review> recentReviews = flatLikeEngine.getRecentReviews();
+
+        assertThat(recentReviews.size(), is(equalTo(10)));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void a_signedin_user_should_be_able_to_add_a_review() {
+        flatLikeEngine.addReview(new Review());
+    }
+
+    @Test
+    public void an_invalid_post_code_should_result_in_an_empty_list_of_properties() {
+        List<Property> properties = flatLikeEngine.getProperties("INVALID POSTCODE");
+
+        assertThat(properties.size(), is(0));
+    }
+
+    @Test
+    public void as_signedin_user_I_should_be_able_to_retrieve_the_overall_review_for_a_particular_property() {
+        List<Property> properties = flatLikeEngine.getProperties("SE16DB");
+
+        Property property = properties.get(0);
+
+        Review review = property.getOverallReview();
+
+        assertThat(review, is(notNullValue()));
+
+        assertThat(review.getRatings(), is(notNullValue()));
+
+        assertThat(review.getRatings().size(), is(greaterThan(0)));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void as_a_signedin_user_I_should_be_able_to_retrieve_individual_reviews() {
+        List<Property> properties = flatLikeEngine.getProperties("SE16DB");
+
+        Property property = properties.get(0);
+
+        List<Review> reviews = property.getReviews();
+    }
+    
+    @Test(expected = UnsupportedOperationException.class)
+    public void as_a_signedin_user_I_should_be_able_comment_reviews() {
+        List<Property> properties = flatLikeEngine.getProperties("SE16DB");
+
+        Property property = properties.get(0);
+
+        List<Review> reviews = property.getReviews();
+    }
+    
+    @Test(expected = UnsupportedOperationException.class)
+    public void as_a_signedin_user_I_should_be_able_to_report_a_review() {
+        List<Property> properties = flatLikeEngine.getProperties("SE16DB");
+
+        Property property = properties.get(0);
+
+        List<Review> reviews = property.getReviews();
+    }
+    
+    @Test(expected = UnsupportedOperationException.class)
+    public void as_a_signedin_user_I_should_be_able_to_vote_if_a_review_is_usefull() {
+        List<Property> properties = flatLikeEngine.getProperties("SE16DB");
+
+        Property property = properties.get(0);
+
+        List<Review> reviews = property.getReviews();
+    }
+}
